ae537e64227612a0b2c6648f1eabc2a3c18c9286
eshamster <hamgoostar@gmail.com>
2019-03-08 14:55:48 +0900

    Add: Model.pm: 登録スキーマ ベタ書きバージョン

--- Jobeet/lib/Jobeet/Models.pm
+++ Jobeet/lib/Jobeet/Models.pm
@@ -3,4 +3,24 @@ use strict;
 use warnings;
 use Ark::Models '-base';
 
+# 今のところは、registerという関数でモデルを登録するということだけ理解しておく。
+# 細部は別の章で見る。
+
+register Schema => sub {
+    my $self = shift;
+
+    my $conf = $self->get('conf')->{database}
+    or die 'require database config';
+
+    $self->ensure_class_loaded('Jobeet::Schema');
+    Jobeet::Schema->connect(@$conf);
+};
+
+for my $table (qw/Job Category CategoryAffiliate Affiliate/) {
+    register "Schema::$table" => sub {
+        my $self = shift;
+        $self->get('Schema')->resultset($table);
+    };
+}
+
 1;
