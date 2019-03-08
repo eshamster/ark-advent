4315e2ac8b0bb26d33a34ee0226224e07eabed17
eshamster <hamgoostar@gmail.com>
2019-03-08 11:41:10 +0900

    Add: insert, update時のcreated_at, udpated_at カラムの自動更新

--- Jobeet/lib/Jobeet/Schema/ResultBase.pm
+++ Jobeet/lib/Jobeet/Schema/ResultBase.pm
@@ -10,5 +10,24 @@ use parent 'DBIx::Class';
 # 少なくともResultとして働くためにはCoreのLoadが必要
 __PACKAGE__->load_components(qw/InflateColumn::DateTime Core/);
 
+sub insert {
+    my $self = shift;
+
+    my $now = Jobeet::Schema->now;
+    $self->created_at( $now ) if $self->can('created_at');
+    $self->updated_at( $now ) if $self->can('updated_at');
+
+    $self->next::method(@_);
+}
+
+sub update {
+    my $self = shift;
+
+    if ($self->can('updated_at')) {
+        $self->updated_at(Jobeet::Schema->now);
+    }
+
+    $self->next::method(@_);
+}
 
 1;
