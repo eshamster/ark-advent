beca0183c719c5c33b8850dfa1cd584a3b9dc20d
eshamster <hamgoostar@gmail.com>
2019-03-11 13:42:38 +0900

    Add: Jobコントーラにアクション群 (空実装) を追加

--- Jobeet/lib/Jobeet/Controller/Job.pm
+++ Jobeet/lib/Jobeet/Controller/Job.pm
@@ -34,6 +34,44 @@ sub index :Path :Args(0) {
     $c->stash->{jobs} = models('Schema::Job');
 }
 
+# /job/{job_token} (詳細)
+sub show :Path :Args(1) {
+    my ($self, $c, $job_token) = @_;
+}
+
+# /job/create (新規作成)
+sub create :Local {
+    my ($self, $c) = @_;
+}
+
+
+# --- 以下 :Chained --- #
+
+# Note:
+# 中間アクション。
+#   - ":Chained" の引数は必ず'/'にする
+#   - ":CaptureArgs" を利用する
+# ":CaptureArgs"をつけることで":Parivate"同様に、
+# 直接アクセスすることはできなくなる。
+# お試し: ":CaptureArgs(2)"とすると、
+# 例えばeditアクションのパスは "/job/*/*/edit"になる
+sub job :Chained('/') :PathPart :CaptureArgs(1) {
+    my ($self, $c, $job_token) = @_;
+    $c->stash->{job_token} = $job_token;
+}
+
+# 末端アクション
+#   - ":Args" を利用する
+# /job/{job_token}/edit (編集) (PathPart = "edit")
+sub edit :Chained('job') :PathPart :Args(0) {
+    my ($self, $c) = @_;
+}
+
+# /job/{job_token}/delete (削除) (PathPart = "delete")
+sub delete :Chained('job') :PathPart :Args(0) {
+    my ($self, $c) = @_;
+}
+
 __PACKAGE__->meta->make_immutable;
 
 1;
