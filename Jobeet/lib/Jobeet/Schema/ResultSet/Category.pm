9e08c3ad15f171d326f58ddaf2c0000d86562dde
eshamster <hamgoostar@gmail.com>
2019-03-11 16:52:16 +0900

    Add: コメントにsearchメソッドのjoinで出力されるSQLを追記

--- Jobeet/lib/Jobeet/Schema/ResultSet/Category.pm
+++ Jobeet/lib/Jobeet/Schema/ResultSet/Category.pm
@@ -9,9 +9,15 @@ use Jobeet::Models;
 sub get_with_jobs {
     my $self = shift;
 
+    # Note: 出力されるSQL
+    # SELECT me.id, me.name, me.slug
+    #   FROM jobeet_category me
+    #     LEFT JOIN jobeet_job jobs
+    #       ON jobs.category_id = me.id
+    #   WHERE ( jobs.expires_at >= ? )
+    #   GROUP BY me.name
     $self->search(
         { 'jobs.expires_at' => { '>=', models('Schema')->now->strftime("%F %T") } },
-        # TODO: 吐き出されるSQLを見る
         { join => 'jobs', group_by => 'me.name' }
     );
 }
