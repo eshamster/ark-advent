eaf7bc1948d08c3472ec6d26626831de9896b69d
eshamster <hamgoostar@gmail.com>
2019-03-11 10:43:41 +0900

    Add: MTビュー, コントローラ

--- Jobeet/lib/Jobeet/Controller.pm
+++ Jobeet/lib/Jobeet/Controller.pm
@@ -15,4 +15,20 @@ sub index :Path :Args(0) {
     $c->res->body('Ark Default Index');
 }
 
+# Note:
+# "Private"の部分はattributes(属性)と呼ばれるもの。
+# importの際に同じ属性の関数をまとめてimportしたりできる。
+# Note:
+# end = コントローラの実行の最後に実行される特別なアクション
+sub end : Private {
+    my ($self, $c) = @_;
+
+    # レスポンスがまだ定義されていない場合
+    unless ($c->res->body or $c->res->status =~ /^3\d\d/) {
+        # MT ビューに処理を配送する
+        $c->forward( $c->view('MT') );
+    }
+}
+
+
 __PACKAGE__->meta->make_immutable;
